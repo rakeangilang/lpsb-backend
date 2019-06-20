4ec764db1a70cf4654321328b2ad03be33d735a1
zakigeyan <zakig013@gmail.com>
2019-06-20 09:42:19 +0700

    Fix layout

--- resources/views/templates/partials/_aside.blade.php
+++ resources/views/templates/partials/_aside.blade.php
@@ -18,7 +18,7 @@
           <ul class="sidebar-menu" data-widget="tree">
             <li class="header">MAIN NAVIGATION</li>
             <li class="active treeview menu-open">
-              <a href="#">
+              <a href="/">
                 <i class="fa fa-dashboard"></i> <span>Dashboard</span>
               </a>
             </li>
