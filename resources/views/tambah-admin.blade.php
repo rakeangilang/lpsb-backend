8ae1744597fde8fca756325dd3f16d89101a728d
vdnrvlrs <valorousidner@gmail.com>
2019-07-03 03:35:33 +0700

    tambahkan admin connect with db

--- resources/views/tambah-admin.blade.php
+++ resources/views/tambah-admin.blade.php
@@ -20,58 +20,94 @@
     <div class="col">
             <!-- Horizontal Form -->
             <div class="box box-default">
-               <div class="box-header with-border">
-               <!-- /.box-header -->
-               <!-- form start -->
-               <form class="form-horizontal">
-               <div class="box-body">
-                    <!-- select -->
-                    <div class="form-group">
-                    <label for="inputEmail3" class="col-sm-3 control-label">Peran Admin</label>
-                    <div class="col-sm-5">
-                    <select class="form-control">
-                        <option value="" selected disabled hidden >Pilih salah satu</option>
-                        <option>Super Admin</option>
-                        <option>Sub Admin</option>
-                    </select>
-                    </div>
-                    </div>
-                    <div class="form-group">
-                                    
-                        <label for="name" class="col-sm-3 control-label">Nama Lengkap</label>
+<div class="panel-body">
+                    <form class="form-horizontal" method="POST" action="{{ route('admin.register.store') }}">
+                        {{ csrf_field() }}
 
-                        <div class="col-sm-5">
-                        <input type="text" class="form-control" id="name">
-                        </div>
-                    </div>
-                    <div class="form-group">
-                        <label for="email" class="col-sm-3 control-label">Alamat Email</label>
+                        <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
+                            <label for="name" class="col-md-4 control-label">Nama</label>
 
-                        <div class="col-sm-5">
-                        <input type="email" class="form-control" id="email">
-                        </div>
-                    </div>
-                    <div class="form-group">
-                        <label for="password" class="col-sm-3 control-label">Password</label>
+                            <div class="col-md-6">
+                                <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
 
-                        <div class="col-sm-5">
-                        <input type="password" class="form-control" id="password">
+                                @if ($errors->has('name'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('name') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
                         </div>
-                    </div>
-                    <div class="form-group">
-                        <label for="confirm_password" class="col-sm-3 control-label">Confirm Password</label>
 
-                        <div class="col-sm-5">
-                        <input type="password" class="form-control" id="confirm_password">
+                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                            <label for="email" class="col-md-4 control-label">E-Mail</label>
+
+                            <div class="col-md-6">
+                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
+
+                                @if ($errors->has('email'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('email') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
                         </div>
-                    </div>
-               </div>
-               <!-- /.box-body -->
-               <div class="box-footer">   
-                  <button type="submit" class="btn btn-info pull-right">Tambah Admin</button>
-               </div>
-               <!-- /.box-footer -->
-               </form>
-            </div>
+                        <div class="form-group{{ $errors->has('jabatan') ? ' has-error' : '' }}">
+                            <label for="jabatan" class="col-md-4 control-label">Jabatan</label>
+
+                            <div class="col-md-6">
+                                <input id="jabatan" type="text" class="form-control" name="jabatan" required>
+
+                                @if ($errors->has('jabatan'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('jabatan') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+                        <div class="form-group{{ $errors->has('ttd') ? ' has-error' : '' }}">
+                            <label for="ttd" class="col-md-4 control-label">Tanda Tangan</label>
+
+                            <div class="col-md-6">
+                                <input id="ttd" type="text" class="form-control" name="ttd" required>
+
+                                @if ($errors->has('ttd'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('ttd') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                            <label for="password" class="col-md-4 control-label">Password</label>
+
+                            <div class="col-md-6">
+                                <input id="password" type="password" class="form-control" name="password" required>
+
+                                @if ($errors->has('password'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('password') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="password-confirm" class="col-md-4 control-label">Confirm Password</label>
+
+                            <div class="col-md-6">
+                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="col-md-6 col-md-offset-4">
+                                <button type="submit" class="btn btn-primary">
+                                    Tambahkan
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
     </section>
 @endsection
+
